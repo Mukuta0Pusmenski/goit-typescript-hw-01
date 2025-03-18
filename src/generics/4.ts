234df2f5ff6e0d1eaa4815234bb3923dfa8d67a4
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-18 16:45:25 +0200

    4.ts=push

---
+++ src/generics/4.ts
@@ -0,0 +1,12 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<User>) {
+  // Логіка оновлення користувача
+  console.log(initialValues);
+}
+
